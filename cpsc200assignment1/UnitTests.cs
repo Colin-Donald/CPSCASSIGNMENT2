e63dacc0bf66f21d293021afa96578b783e0415c
Colin Donald <colinjdonald@hotmail.ca>
2014-10-22 12:22:23 -0700

    unittests......

--- cpsc200assignment1/UnitTests.cs
+++ cpsc200assignment1/UnitTests.cs
@@ -16,7 +16,11 @@ namespace cpsc200assignment1
         private TestInsertionSort IS;
         private TestSelectionSort SS;
         private TestShellSort SHS;
-        private int[] list = new int[10] {10,5,3,6,7,2,4,8,1,9};
+        private int[] list = new int[10] { 10, 5, 3, 6, 7, 2, 4, 8, 1, 9 };
+        private int[] reverselist = new int[10] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        private int[] distinctlist = new int[10] { 0, 4, 3, 2, 2, 1, 4, 5, 3, 2 };
+        private int[] listAns = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        private int[] distinctlistAns = new int[10] { 0, 1, 2, 2, 2, 3, 3, 4, 4, 5 };
         public UnitTests()
         {
             eShell = new TestExperimentParams();
